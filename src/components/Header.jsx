cdc68de06256bd1919048a0fb4ffbd6971548611
ammurana4216 <drona.ambeshwar@gmail.com>
2023-07-14 12:08:56 +0530

    header edit done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { truncateText } from '../utlility/utils';
 
 function Header(props) {
+    
     const { video } = props
     console.log(video);
 
--- src/utlility/requests.js
+++ src/utlility/requests.js
@@ -2,5 +2,5 @@ const API_KEY = 'd1e04095add9191679d32eaef7568e3d';
 
 export const requests ={
     getNetflixOrignals: `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    getPopularMovies:`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&with_networks=213`
+    getPopularMovies:`movie/popular?api_key=${API_KEY}&language=en-US&page=1`
 }
